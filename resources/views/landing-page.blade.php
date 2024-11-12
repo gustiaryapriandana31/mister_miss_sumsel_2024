5d17e0eac2a016a6c570bb92825bc74e2f414017
gustiaryapriandana31 <gustiaryapriandana@gmail.com>
2024-11-12 20:02:58 +0700

    feat:initialize and installation project and all of dependencies

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <title>Landing Page Mister and Miss Sumsel 2024</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1 class="text-center text-sky-300 font-bold text-3xl">MISTER & MISS SUMSEL 2024</h1>
+</body>
+</html>
